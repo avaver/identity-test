409b4565d32cb1a22333d25869622a0cf0a00641
avaver <avaver@gmail.com>
2021-02-02 12:11:25 +0200

    many changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
-import './App.css';
+import { useEffect } from 'react';
 import { useAuth } from './auth/AuthProvider';
 import config from './authConfig';
 
 function App() {
   const { user, login, logout } = useAuth();
+
   const loginLogout = () => {
     if (user) logout();
     else login();
   };
 
+  useEffect(() => {
+    document.title = config.resolveTenant() + ' | DentalSuite Nexta';
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ padding: '20px' }}>
-          User: {user ? user.id + ' (' + user.name + ' @ ' + user.tenant + ')' : 'N/A'}
-        </div>
-        <button onClick={() => loginLogout()}>{user ? 'Logout' : 'Login to ' + config.resolveTenant()}</button>
-      </header>
+    <div className="text-center content-center-outer">
+      <div className="content-center-inner">
+        <h1 className="h2 mb-5">Welcome to {config.resolveTenant()}</h1>
+        {user && (
+          <h1 className="h5 mb-3">
+            You are currently logged in as <span className="text-success">{user.name}</span>
+          </h1>
+        )}
+        {user ? (
+          <div className="row">
+            <div className="col">
+              <button
+                className="btn btn-dark"
+                onClick={() => (window.location.href = 'https://account.dentalsuite.local:5001/account')}
+              >
+                Account
+              </button>
+            </div>
+            <div className="col">
+              <button className="btn btn-dark" onClick={() => loginLogout()}>
+                Sign out
+              </button>
+            </div>
+          </div>
+        ) : (
+          <button className="btn btn-dark" onClick={() => loginLogout()}>
+            Sign in
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,12 +1,12 @@
 import { MultitenantUserManagerSettings } from './auth/types';
 
 const config: MultitenantUserManagerSettings = {
-  authority: 'https://localhost:5001/',
+  authority: 'https://account.dentalsuite.local:5001/',
   client_id: 'dentalsuite.web',
   redirect_uri: `${window.location.origin}/authentication/callback`,
   silent_redirect_uri: `${window.location.origin}/authentication/silentcallback`,
   response_type: 'code',
-  scope: 'openid profile dentalsuite.api',
+  scope: 'openid dentalsuite.profile dentalsuite.api',
   post_logout_redirect_uri: `${window.location.origin}/`,
   automaticSilentRenew: true,
   revokeAccessTokenOnSignout: true,
